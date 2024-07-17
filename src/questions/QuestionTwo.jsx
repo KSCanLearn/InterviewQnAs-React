bb9063c8f97d29038752910b3d24979888bd4a40
KSCanLearn <kscanlearn@gmail.com>
2024-07-17 12:09:10 +0800

    Answer for Question Two

--- src/questions/QuestionTwo.jsx
+++ src/questions/QuestionTwo.jsx
@@ -2,23 +2,47 @@
 
 /**
  * Debouce Hook Tester
+ * Expected Outcome: Have a delay to display the debounced value
  */
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
+
+// by right should be in another file
+const useDebounce = (text = "", delay = 1000) => {
+	const [debounceText, setDebounceText] = useState(text);
+	const [isLoad, setIsLoad] = useState(false);
+
+	useEffect(() => {
+		setIsLoad(true);
+		const timer = setTimeout(() => {
+			// Fetch API call to get something
+			// Refer to this: https://www.youtube.com/watch?v=VdAlFWBUGV4 for having AbortController
+			setDebounceText(text);
+			setIsLoad(false);
+		}, delay);
+
+		return () => {
+			clearTimeout(timer);
+			setIsLoad(false);
+		};
+	}, [text, delay]);
+
+	return [debounceText, isLoad];
+};
 
 const QuestionTwo = () => {
-	const [text, setText] = useState();
+	const [text, setText] = useState("");
+	const [debounceText, isLoad] = useDebounce(text, 2000);
 
 	return (
 		<div>
 			<h2>Debounce Hook Tester</h2>
 			<input
 				type="text"
-				value={text}
 				onChange={(e) => setText(e.target.value)}
 				placeholder="Type something..."
 			/>
-			<p>Debounced value: </p>
+			<p>Debounced value: {text.length > 0 && isLoad ? "Waiting..." : debounceText}</p>
 		</div>
 	);
 };
